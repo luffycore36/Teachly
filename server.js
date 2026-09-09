2a3dc5a3281295f8f009e8ba92cf31bff9208e98
luffycore36 <luffycore36@gmail.com>
2026-09-09 04:34:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,17 @@ const nodemailer = require("nodemailer");
 dotenv.config();
 
 const app = express();
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(204);
+    }
+
+    next();
+});
 const server = http.createServer(app);
 const io = new Server(server);
 
